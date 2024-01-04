c42eb03368a485e4c9cd7055dcf746a81853c330
Kanev Aleksandr <kanev085@yandex.ru>
2024-01-04 14:00:37 +0300

    дз_6

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from src.hw_3.task_8 import app
+# from src.hw_3.task_8 import app
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import uvicorn
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    uvicorn.run('hw_6.task_6:app', port=80, reload=True)
